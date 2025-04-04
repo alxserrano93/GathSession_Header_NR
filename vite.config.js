2e9eedf345c90d15899d4f9f9297608c69509cd4
Alex <alexserr.dev@gmail.com>
2025-04-04 18:52:34 +0200

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/GathSession_Header_NR/", // Usa rutas relativas en los enlaces generados en el HTML
+
+  build: {
+    outDir: "dist", // Asegura que la carpeta de salida es "dist"
+    assetsDir: "assets", // Mantiene los archivos JS y CSS en "dist/assets"
+  },
+
+  server: {
+    watch: {
+      usePolling: true, // Activa la detección de cambios en ciertos entornos
+    },
+    host: true, // Permite acceder desde otras IPs (útil en WSL o Docker)
+    strictPort: true, // Evita que cambie de puerto automáticamente
+  },
+});
